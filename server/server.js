fdb14674acce7eb6abf8922a5af2b6031492b7cd
Sudha Krishnamoorthy <sudhavkr@hotmail.com>
2022-11-01 21:36:21 -0500

    commit deployment-demo

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/index.html"));
-});
+//app.get("/", function(req, res) {
+  //  res.sendFile(path.join(__dirname, "/../public/index.html"));
+//});
+
+app.use(express.static(path.join(__dirname, '../public')))
 
 const port = process.send.PORT || 4005;
 
